f1333965b05b1153fd9ee73b8d943035d1d6a936
WandsonB <108149915+WandsonB@users.noreply.github.com>
2023-09-10 16:47:28 -0300

    0.2.4 rename mainActivity

--- app/src/main/java/com/swt/qualifica_2_ciclo/splashActivity.kt
+++ app/src/main/java/com/swt/qualifica_2_ciclo/splashActivity.kt
@@ -14,7 +14,7 @@ class splashActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
             finish()} , 2000)
     }
