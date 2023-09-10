ef1d0eb1d826217936c03f1defec8529f9862f08
WandsonB <108149915+WandsonB@users.noreply.github.com>
2023-09-10 17:54:17 -0300

    0.3.1 create layout activity Login and register
    create layout activity Login and register  and set Intent

--- app/src/main/java/com/swt/qualifica_2_ciclo/LoginActivity.kt
+++ app/src/main/java/com/swt/qualifica_2_ciclo/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.swt.qualifica_2_ciclo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            goRegister()
+        }
+
+        val btnLostPass = findViewById<Button>(R.id.btnLostPass)
+        btnLostPass.setOnClickListener {
+            goRegister()
+        }
+    }
+
+    private fun goRegister() {
+        val goRegister = Intent(this, registerActivity::class.java)
+        startActivity(goRegister)
     }
 }
