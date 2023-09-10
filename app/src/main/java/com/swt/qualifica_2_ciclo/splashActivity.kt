0aeaccad04d33d56f28f8f3f8214652f9ed19396
WandsonB <108149915+WandsonB@users.noreply.github.com>
2023-09-10 00:00:28 -0300

    Initial commit

---
+++ app/src/main/java/com/swt/qualifica_2_ciclo/splashActivity.kt
@@ -0,0 +1,21 @@
+package com.swt.qualifica_2_ciclo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()} , 2000)
+    }
+}
