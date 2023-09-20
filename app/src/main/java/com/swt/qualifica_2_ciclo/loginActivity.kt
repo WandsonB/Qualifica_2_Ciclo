8fb80d18aab1e36f2bd3552dd91bec2560175ede
WandsonB <108149915+WandsonB@users.noreply.github.com>
2023-09-20 12:34:36 -0300

    0.4.2 Login verification

--- app/src/main/java/com/swt/qualifica_2_ciclo/loginActivity.kt
+++ app/src/main/java/com/swt/qualifica_2_ciclo/loginActivity.kt
@@ -3,12 +3,35 @@ package com.swt.qualifica_2_ciclo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        val lgnEmail = findViewById<EditText>(R.id.editTextEmail).toString()
+        val lgnPass = findViewById<EditText>(R.id.editTextPass).toString()
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        val extName = intent.getStringExtra("name")
+        val extEmail = intent.getStringExtra("email")
+        val extPass = intent.getStringExtra("pass")
+
+        btnLogin.setOnClickListener(View.OnClickListener{
+            val extStrEmail = extEmail.toString()
+            val extStrPass = extPass.toString()
+
+            if (lgnEmail == extStrEmail && lgnPass == extStrPass) {
+                Toast.makeText(this, "Login bem-sucedido! \n Bem-vindo ${extName}", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Email ou senha incorretos.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
         val btnRegister = findViewById<Button>(R.id.btnRegister)
         btnRegister.setOnClickListener {
